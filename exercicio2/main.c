562e593e553936cecf2b9cef833cbdabd29d0497
Sousadudux <dudu.soliveira78@gmail.com>
2026-01-23 20:28:08 -0300

    exercicio3

---
+++ exercicio2/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main() 
+{
+    for (int i = 1; i <= 100; i++)
+    { printf("%i\n", i);
+           
+}
+    
+while () { 
+
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    return 0; }
---
+++ exercicio3/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main()
+{
+ for (int i = 0; i <=100000; i+=1000)
+ {
+    printf("Soma: %d\n", i);
+ }
+ 
+   
+
+
+
+
+
+
+return 0;}
