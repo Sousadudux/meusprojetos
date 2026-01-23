ae19d171557dfa5e8ae23a60f971225eda8bf631
Sousadudux <dudu.soliveira78@gmail.com>
2026-01-23 19:46:48 -0300

    exercicios

---
+++ exercicios/exercicio 1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main() 
+    { 
+    for (int i = 3; i <= 15; i += 3) {
+        printf(" = %d\n", i);    
+        
+    }   
+    return 0;
+}
