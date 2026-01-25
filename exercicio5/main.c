dfa21a39872c0f2dfb97fd5dc0f32dcc821c27ef
Sousadudux <dudu.soliveira78@gmail.com>
2026-01-24 22:28:16 -0300

    exercicio5

---
+++ exercicio5/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int total, soma=0;
+    for (int i = 1; i<=10; i++) {
+        printf("Digite o %d valor\n", i);
+        scanf("%d", &total);
+        soma += total;
+        printf("A soma atual é: %d\n", soma);
+    }
+    
+    return 0;
+}
