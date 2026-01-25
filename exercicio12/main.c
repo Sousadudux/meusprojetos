cd06a37001a3489bf1f78aa21c60c55576c0289a
Sousadudux <dudu.soliveira78@gmail.com>
2026-01-25 01:11:59 -0300

    exercicio12

---
+++ exercicio12/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+int numero;
+
+printf("escreva o numero final!!\n=");
+scanf("%d", &numero);
+
+ for (int i = 0; i <= numero; i++) {
+
+    printf("\n %d", i);
+
+ }
+
+return 0; }
